fc6f7034f5a70e2a7f7d7f2ea5063a0dfa9296a1
phlabsdevops <harinathbeeram1999@gmail.com>
2025-06-03 18:44:40 +0530

    terraform modules perfect example

--- roboshop-ec2/variables.tf
+++ roboshop-ec2/variables.tf
@@ -2,3 +2,12 @@ variable "instance_size" {
   type=string
   default="t2.large"
 }
+variable "tags" {
+  type=map
+  default={
+    Name = "HelloTerraform"
+    Project="Roboshop"
+    Environment="DEV"
+    Terraform=true
+  }
+}
